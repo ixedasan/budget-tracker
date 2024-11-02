597112435bdc58ee64542f04c831e488edd47a2f
ixedasan <pandikgenshin@gmail.com>
2024-11-03 00:30:52 +0200

    feat(dashboard): add create transactions dialog

---
+++ src/app/(dashboard)/_components/CategorySelector.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const CategorySelector = (props: Props) => {
+  return <div>CategorySelector</div>
+}
+
+export default CategorySelector
---
+++ src/app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import { ReactNode } from 'react'
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from '@/schema/transaction'
+import { TransactionType } from '@/types'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { cn } from '@/lib/utils'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+import CategorySelector from './CategorySelector'
+
+type Props = {
+  trigger: ReactNode
+  type: TransactionType
+}
+
+const CreateTransactionDialog = ({ trigger, type }: Props) => {
+  const form = useForm<CreateTransactionSchemaType>({
+    resolver: zodResolver(CreateTransactionSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  })
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            New{' '}
+            <span
+              className={cn(
+                type === 'income'
+                  ? 'text-green-500'
+                  : type === 'expense' && 'text-red-500',
+                'm-1',
+              )}
+            >
+              {type}
+            </span>
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={''} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={0} type="number" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction amount (required)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center justify-between gap-2">
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <CategorySelector />
+                    </FormControl>
+                    <FormDescription>
+                      Select transaction category
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default CreateTransactionDialog
