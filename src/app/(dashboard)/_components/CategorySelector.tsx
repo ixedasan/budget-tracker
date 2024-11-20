c34f1576ad6d6cbe0bf710a89d45b6f87633eab3
ixedasan <pandikgenshin@gmail.com>
2024-11-21 01:19:13 +0200

    feat(category): add fetch view

--- src/app/(dashboard)/_components/CategorySelector.tsx
+++ src/app/(dashboard)/_components/CategorySelector.tsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import { TransactionType } from '@/types'
 import { Category } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
+import { Check, ChevronDown } from 'lucide-react'
 
+import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { Command, CommandInput } from '@/components/ui/command'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
 import {
   Popover,
   PopoverContent,
@@ -46,12 +55,40 @@ const CategorySelector = ({ type }: Props) => {
           ) : (
             'Select category'
           )}
+          <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-52 p-0">
         <Command onSubmit={e => e.preventDefault()}>
           <CommandInput placeholder="Search category..." />
           <CreateCategoryDialog type={type} />
+          <CommandEmpty>
+            <p>Category not found</p>
+            <p className="text-xs text-muted-foreground">
+              Tip: You can create a new category
+            </p>
+          </CommandEmpty>
+          <CommandGroup>
+            <CommandList>
+              {categiriesQuery.data?.map((category: Category) => (
+                <CommandItem
+                  key={category.name}
+                  onSelect={() => {
+                    setValue(category.name)
+                    setOpen(false)
+                  }}
+                >
+                  <CategoryRow category={category} />
+                  <Check
+                    className={cn(
+                      'h-4 w-4 opacity-0',
+                      value === category.name && 'opacity-100',
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandList>
+          </CommandGroup>
         </Command>
       </PopoverContent>
     </Popover>
