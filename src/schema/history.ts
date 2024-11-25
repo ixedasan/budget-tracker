6e4b67fee4ed8eb4cc4bf758449c2367205e1180
ixedasan <pandikgenshin@gmail.com>
2024-11-25 22:30:43 +0200

    fix(api): fix january selection error

--- src/schema/history.ts
+++ src/schema/history.ts
@@ -2,6 +2,6 @@ import { z } from 'zod'
 
 export const getHistoryDataSchema = z.object({
   interval: z.enum(['month', 'year']),
-  month: z.coerce.number().min(1).max(12),
+  month: z.coerce.number().min(0).max(11),
   year: z.coerce.number().min(2000).max(2200),
 })
