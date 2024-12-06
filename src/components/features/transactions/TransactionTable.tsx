00d9a81904c69eb471d6b3b804b6ba16b5e26c1c
ixedasan <pandikgenshin@gmail.com>
2024-12-06 23:57:01 +0200

    feat(transactions): add filters and complete table

--- src/components/features/transactions/TransactionTable.tsx
+++ src/components/features/transactions/TransactionTable.tsx
@@ -1,17 +1,21 @@
 'use client'
 
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { dateToUTC } from '@/helpers/date-to-utc'
 import { useQuery } from '@tanstack/react-query'
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   SortingState,
   useReactTable,
 } from '@tanstack/react-table'
 
+import { cn } from '@/lib/utils'
 import {
   Table,
   TableBody,
@@ -24,6 +28,8 @@ import SkeletonWrapper from '@/components/feedback/SkeletonWrapper'
 import { getTransactionsResponseType } from '@/app/api/transactions/route'
 
 import { DataTableColumnHeader } from './tables/ColumnHeader'
+import { DataTableViewOptions } from './tables/ColumnToggle'
+import { DataTableFacetedFilter } from './tables/Filters'
 
 type Props = {
   from: Date
@@ -41,6 +47,9 @@ export const columns: ColumnDef<TransactiHistoryRow>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Category" />
     ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    },
     cell: ({ row }) => (
       <div className="flex gap-2 capitalize">
         {row.original.categoryIcon}
@@ -48,10 +57,70 @@ export const columns: ColumnDef<TransactiHistoryRow>[] = [
       </div>
     ),
   },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Description" />
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">
+        {row.original.description || 'No description'}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'date',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Date" />
+    ),
+    cell: ({ row }) => {
+      const date = new Date(row.original.date)
+      const formattedDate = date.toLocaleDateString('default', {
+        timeZone: 'UTC',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      return <div className="text-muted-foreground">{formattedDate}</div>
+    },
+  },
+  {
+    accessorKey: 'type',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Type" />
+    ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    },
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          'rounded-lg p-2 text-center capitalize',
+          row.original.type === 'expense'
+            ? 'bg-red-400/10 text-red-500'
+            : 'bg-green-400/10 text-green-500',
+        )}
+      >
+        {row.original.type}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'amount',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Amount" />
+    ),
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-center font-medium">
+        {row.original.formattedAmount}
+      </p>
+    ),
+  },
 ]
 
 const TransactionTable = ({ from, to }: Props) => {
   const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
   const historyQuery = useQuery<getTransactionsResponseType>({
     queryKey: ['transactions', 'history', from, to],
@@ -67,14 +136,53 @@ const TransactionTable = ({ from, to }: Props) => {
     getCoreRowModel: getCoreRowModel(),
     state: {
       sorting,
+      columnFilters,
     },
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
   })
+
+  const categoriesOptions = useMemo(() => {
+    const categoriesMap = new Map()
+    historyQuery.data?.forEach(transaction => {
+      categoriesMap.set(transaction.category, {
+        label: `${transaction.categoryIcon} ${transaction.category}`,
+        value: transaction.category,
+      })
+    })
+
+    const unicCategories = new Set(categoriesMap.values())
+    return Array.from(unicCategories)
+  }, [historyQuery.data])
 
   return (
     <div className="w-full">
-      <div className="flex flex-wrap items-center justify-between py-4"></div>
+      <div className="flex flex-wrap items-center justify-between py-4">
+        <div className="flex gap-2">
+          {table.getColumn('category') && (
+            <DataTableFacetedFilter
+              title="Category"
+              column={table.getColumn('category')}
+              options={categoriesOptions}
+            />
+          )}
+          {table.getColumn('type') && (
+            <DataTableFacetedFilter
+              title="Type"
+              column={table.getColumn('type')}
+              options={[
+                { label: 'Income', value: 'income' },
+                { label: 'Expense', value: 'expense' },
+              ]}
+            />
+          )}
+        </div>
+        <div className="flex flex-wrap gap-2">
+          <DataTableViewOptions table={table} />
+        </div>
+      </div>
       <SkeletonWrapper isLoading={historyQuery.isFetching}>
         <div className="rounded-md border">
           <Table>
